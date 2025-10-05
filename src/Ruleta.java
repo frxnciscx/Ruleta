d1aa43919e4bd9c33047df8dea4a0bfe5933f7ed
francisca <f.aguayo03@ufromail.cl>
2025-10-04 22:58:38 -0300

    Agregar sistema de historial con arrays estaticos para ruleta

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -1,4 +1,13 @@
 public class Ruleta {
 
+    //constantes y arreglos para guardar historial
+    public static final int MAX_HISTORIAL = 100;
+    public static int[] historialNumeros = new int[MAX_HISTORIAL];
+    public static int[] historialApuestas = new int[MAX_HISTORIAL];
+    public static boolean[] historialAciertos = new boolean[MAX_HISTORIAL];
+    public static int historialSize = 0;
+
+
+
 }
 
