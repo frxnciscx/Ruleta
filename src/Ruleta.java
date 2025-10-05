3fddb5a0f07a2b938c93d22e3cdfe3cbcaac4c91
francisca <f.aguayo03@ufromail.cl>
2025-10-04 23:05:28 -0300

    Implementar menu principal interactivo para juego de Ruleta

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Ruleta {
 
@@ -14,6 +15,20 @@ public class Ruleta {
     //numeros rojos de la ruleta
     public static int[] numerosRojos = {1,3,5,7,9,12,14,16,18,19,21,23,27,30,32,34,36};
 
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu(){
+        Scanner in = new Scanner(System.in);
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = leerOpcion(in);
+            ejecutarOpcion(opcion, in);
+        } while (opcion != 3);
+        in.close();
+    }
 
 
 
