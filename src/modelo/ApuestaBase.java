7fff6a1ed91b36f62a59a71bad10c9a9f769f68a
Fr4mbu <f.aguayo03@ufromail.cl>
2025-11-07 09:16:50 -0300

    Crear clase abstracta ApuestaBase para sistema polimórfico de apuestas

---
+++ src/modelo/ApuestaBase.java
@@ -0,0 +1,22 @@
+package modelo;
+
+public abstract class ApuestaBase {
+
+    protected int monto;
+    protected String etiqueta;
+
+    public ApuestaBase(int monto, String etiqueta) {
+        this.monto = monto;
+        this.etiqueta = etiqueta;
+    }
+
+    public abstract boolean acierta(int numero, String color);
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+}
