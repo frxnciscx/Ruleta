0149089bbe623ceb446eac8f7cf6c462135acc98
Fr4mbu <f.aguayo03@ufromail.cl>
2025-11-07 09:30:44 -0300

    Implementar ApuestaRojo como clase serializable

--- src/modelo/ApuestaRojo.java
+++ src/modelo/ApuestaRojo.java
@@ -1,13 +1,7 @@
 package modelo;
 
 public class ApuestaRojo extends ApuestaBase {
-
-    public ApuestaRojo(int monto) {
-        super(monto, "Rojo");
-    }
-
+    public ApuestaRojo(int monto) { super(monto, "Rojo"); }
     @Override
-    public boolean acierta(int numero, String color) {
-        return color.equals("ROJO");
-    }
+    public boolean acierta(int numero, String color) { return color.equals("ROJO"); }
 }
