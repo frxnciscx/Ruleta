60aa6c4c7b47e14597c368a007caa17bc11f89f8
Fr4mbu <f.aguayo03@ufromail.cl>
2025-11-07 09:32:08 -0300

    Implementar ApuestaPar como clase serializable

--- src/modelo/ApuestaPar.java
+++ src/modelo/ApuestaPar.java
@@ -1,13 +1,7 @@
 package modelo;
 
 public class ApuestaPar extends ApuestaBase {
-
-    public ApuestaPar(int monto) {
-        super(monto, "Par");
-    }
-
+    public ApuestaPar(int monto) { super(monto, "Par"); }
     @Override
-    public boolean acierta(int numero, String color) {
-        return numero != 0 && numero % 2 == 0;
-    }
+    public boolean acierta(int numero, String color) { return numero != 0 && numero % 2 == 0; }
 }
