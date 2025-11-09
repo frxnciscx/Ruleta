4d1636540c898e01bc246bcb151a09e1efbca1c6
Fr4mbu <f.aguayo03@ufromail.cl>
2025-11-09 17:44:32 -0300

    Refactorizar RuletaController para usar IRepositorioResultados

--- src/controlador/RuletaController.java
+++ src/controlador/RuletaController.java
@@ -10,9 +10,11 @@ public class RuletaController {
     private final SessionController session;
     private final Usuario usuario;
     private final Ruleta ruleta;
+    private final IRepositorioResultados repositorio;
 
-    public RuletaController(SessionController session) {
+    public RuletaController(SessionController session, IRepositorioResultados repositorio) {
         this.session = session;
+        this.repositorio = repositorio;
         this.usuario = session.getUsuarioActual();
         this.ruleta = new Ruleta(usuario.getSaldo());
     }
@@ -28,12 +30,20 @@ public class RuletaController {
         usuario.actualizarSaldo(ganancia);
 
         Resultado res = new Resultado(numero, apuesta, acierto, saldoDespues);
-        usuario.agregarResultado(res);
+
+        this.repositorio.agregarResultado(res);
     }
 
-    // Getters para la Vista
-    public int getUltimoNumero() { return ruleta.getUltimoNumero(); }
-    public String getUltimoColor() { return ruleta.getUltimoColor(); }
-    public boolean getUltimoAcierto() { return ruleta.getUltimoAcierto(); }
-    public int getSaldoActual() { return usuario.getSaldo(); }
+    public int getUltimoNumero() {
+        return ruleta.getUltimoNumero();
+    }
+    public String getUltimoColor() {
+        return ruleta.getUltimoColor();
+    }
+    public boolean getUltimoAcierto() {
+        return ruleta.getUltimoAcierto();
+    }
+    public int getSaldoActual() {
+        return usuario.getSaldo();
+    }
 }
